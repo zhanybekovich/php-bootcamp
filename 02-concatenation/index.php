99d210d6168d69208e50610d5f2b75938427583d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-06 21:31:50 +0600

    Concatenation & string interpolation & escape character

---
+++ 02-concatenation/index.php
@@ -0,0 +1,19 @@
+<?php
+// concatenation
+$firstName = 'John';
+$lastName = 'Doe';
+$fullName = $firstName . " " . $lastName;
+
+echo $fullName;
+
+echo "<hr>";
+
+// string interpolation
+$userFirstName = "John";
+$userLastName = "Doe";
+echo "Hello, $userFirstName $userLastName!";
+
+echo "<hr>";
+
+// escape character
+echo 'I\'m John Doe';
