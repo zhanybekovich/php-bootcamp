0e8a7f5bdded609c54988b8badbe807685e28dd1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-09 23:23:42 +0600

    Delete

---
+++ 45-delete/post.php
@@ -0,0 +1,51 @@
+<?php
+require 'database.php';
+
+$id = $_GET['id'] ?? null;
+
+// echo $id;
+
+if (!$id) {
+    header('Location: index.php');
+}
+
+$sql = 'SELECT * FROM posts WHERE id = :id';
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute(['id' => $id]);
+
+$post = $stmt->fetch();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $post['title'] ?></title>
+</head>
+
+<body>
+
+    <h1><?= $post['title'] ?></h1>
+
+    <div>
+        <p>
+            <?= $post['body'] ?>
+        </p>
+    </div>
+
+    <div>
+        <form action="delete.php" method="post">
+            <input type="hidden" name="_method" value="delete">
+            <input type="hidden" name="id" value="<?= $post['id'] ?>">
+            <button type="submit" name="submit">Delete</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
