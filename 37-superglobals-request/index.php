26ae3963c26e856e30f510d0fe86cbc1f3e9e766
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-06-09 12:53:34 +0600

    Superglobals: request

---
+++ 37-superglobals-request/index.php
@@ -0,0 +1,7 @@
+<?php
+// NOT SECURE
+// $_REQUEST - contains the content of $_GET, $_POST and $_COOKIE (NOT SECURE!)
+
+
+// http://php-bootcamp.test/37-superglobals-request?name=tom
+echo $_REQUEST['name'];
